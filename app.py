c5169a1780a8a5e9bd59e51a3afc175a4c71d3b9
Najmus Saquib <saquibshaikh433@gmail.com>
2022-09-25 15:59:29 +0530

    add profile with requirements file

--- app.py
+++ app.py
@@ -13,17 +13,27 @@ channel_dict = pickle.load(open(pkl_path+"/"+"channel.pkl","rb"))
 year_dict = pickle.load(open(pkl_path+"/"+"year.pkl", "rb"))
 sc = pickle.load(open(pkl_path+"/"+"sc.pkl","rb"))
 
-
 @app.route("/")
 def home():
     return render_template("home.html")
 
+
 @app.route("/predict", methods=["POST"])
+
 def predict():
     df = request.json["data"]
     new = sc.transform(np.array(list(df.values())).reshape(1,-1))
     out = model.predict(new)
     return jsonify(out[0])
+
+
+@app.route("/prediction", methods=["POST"])
+def prediction():
+    df = [int(x) for x in request.form.values()]
+    print(df)
+    input = sc.transform(np.array(df).reshape(1,-1))
+    output = model.predict(input)[0]
+    return render_template("home.html", show_results="prediction sessions on given situation is {}".format(output))
 
 if __name__=="__main__":
     app.run(debug=True)
